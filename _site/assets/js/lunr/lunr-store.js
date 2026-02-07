f9288b668d97582a4c496863e86abefa87aa0c5a
spinal-one <spinalcomputer@gmail.com>
2026-02-07 20:43:05 +0100

    admin

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "The Correct Kind of Work",
-        "excerpt":"The most profitable and timetested money-making system that exists is Communism. Compared to Communism, alternative systems crumble under pressure and invite unwanted disorder, especially in moneyed environments. It is the only system fot which there is undisputed evidence of high profitability and minimal downside. This essay will not only explain...","categories": ["Funnies"],
+        "excerpt":"The most profitable and timetested money-making system in the world is Communism. Compared to Communism, alternative systems crumble under pressure and invite unwanted disorder, especially in moneyed environments. It is the only system for which there is undisputed evidence of high profitability and minimal downside. This essay will not only...","categories": ["Funnies"],
         "tags": ["Fiction"],
         "url": "/funnies/correct-kind-of-work/",
         "teaser": null
@@ -9,5 +9,11 @@ var store = [{
         "excerpt":"The following is an attempt to propose a method for loading standardized, readable data onto blockchain networks. Introduction A certain demand exists for the permanence, traceability, and publicity of data records; one that spans across a great number of domains, including those of the enterprise, the government, all the way...","categories": ["Design"],
         "tags": ["Technical"],
         "url": "/design/LARP/",
+        "teaser": null
+      },{
+        "title": "In abandoned waterpark",
+        "excerpt":"A bald man heftly built throws you in the water off the ramp, along with a man-sized iron arrow you must catch in fall. The man don’t ask but throw, as you balance into the routine from ignorance total. By the third throw, resistance mild and mind begin to go....","categories": ["Funnies"],
+        "tags": ["Short"],
+        "url": "/funnies/abandoned-waterpark/",
         "teaser": null
       }]
